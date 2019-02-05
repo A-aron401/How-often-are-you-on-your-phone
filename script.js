84515c7e22f15075680ec1e049cbb418ad737d19
A-aron401 <43795710+A-aron401@users.noreply.github.com>
2019-02-05 14:31:10 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,14 @@
+$("button").click(function() {
+    var name=$(".names").val();
+    var hours=parseInt($(".hour").val());  
+     var phones=$(".phone").val();
+     var answer=(hours*11315);
+     var result= (name+" you will use your "+phones+" for "+answer+" hours by 2050");
+     
+      $("p").text(result);
+});
+
+
+
+
+    
